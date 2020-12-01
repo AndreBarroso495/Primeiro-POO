065d511100875892421ab7fa75fa15ebf0096f46
AndreBarroso495 <73665987+AndreBarroso495@users.noreply.github.com>
2020-12-01 10:50:06 -0300

    Projeto concluído

---
+++ Personagem.cs
@@ -0,0 +1,16 @@
+namespace Atributos_e_métodos
+{
+    public class Personagem
+    {
+        public string nome;
+
+        public int idade;
+
+        public string armadura;
+
+        public string Ataque(){
+            return "Acertou";
+        }
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Atributos_e_métodos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Personagem objetoPersonagem1 = new Personagem();
+
+            objetoPersonagem1.nome = "Seiya";
+            objetoPersonagem1.idade = 13;
+            objetoPersonagem1.armadura = "Armadura de Pégaso";
+        
+            Personagem objetoPersonagem2 = new Personagem();
+
+            objetoPersonagem2.nome = "Ikki";
+            objetoPersonagem2.idade = 15;
+            objetoPersonagem2.armadura = "Armadura de Fenix";
+
+            Console.WriteLine($"Personagem 1 = {objetoPersonagem1.nome}");
+            Console.WriteLine($"Personagem 2 = {objetoPersonagem2.nome}");
+        }    
+        
+            
+    }
+}
